21a7ed1cc59f943574a7ee97e16c4e67399d0ab8
Hikka Modules Central <157649648+hcmod1@users.noreply.github.com>
2024-08-05 05:31:06 +0300

    Update autoreplymod.py

--- autoreplymod.py
+++ autoreplymod.py
@@ -2,6 +2,9 @@ __version__ = (1, 0, 0)
 
 # 💾 Code is licensed under CC-BY-NC-ND 4.0 unless otherwise specified.
 # 🔒 https://creativecommons.org/licenses/by-nc-nd/4.0/
+# + attribution
+# + non-commercial
+# + no-derivatives
 
 # You CANNOT edit this file without direct permission from the author.
 # You can redistribute this file without any changes.
