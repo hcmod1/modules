223238da14a0a96848b4695cfff216413cd3b125
Hikka Modules Central <157649648+hcmod1@users.noreply.github.com>
2024-08-09 00:37:23 +0300

    Update botcheck.py

--- botcheck.py
+++ botcheck.py
@@ -1,4 +1,4 @@
-__version__ = (1, 0, 0)
+version = (1, 0, 0)
 
 # 🔒 Licensed under the GNU AGPLv3
 # 🌐 https://www.gnu.org/licenses/agpl-3.0.html
@@ -7,68 +7,69 @@ __version__ = (1, 0, 0)
 # scope: hikka_only
 # scope: hikka_min 1.2.10
 
+import datetime
 from telethon.tl.types import Message
 from .. import loader, utils
 
 @loader.tds
 class BotCheck(loader.Module):
     """Module to check if the userbot is working."""
-
+    
     strings = {
         "name": "BotCheck",
-        "bot_response": "<emoji document_id=5206607081334906820>✔️</emoji> <b>Userbot is working!</b>"
+        "bot_response": "<emoji document_id=5206607081334906820>✔️</emoji> <b>Userbot is working!</b>\n<i>~ {uptime}</i>"
     }
 
     strings_ru = {
-        "bot_response": "<emoji document_id=5206607081334906820>✔️</emoji> <b>Юзербот работает!</b>",
+        "bot_response": "<emoji document_id=5206607081334906820>✔️</emoji> <b>Юзербот работает!</b>\n<i>~ {uptime}</i>",
         "_cls_doc": "Модуль для проверки работоспособности юзербота.",
         "_cmd_doc_bot": "- используйте эту команду для проверки работоспособности."
     }
 
     strings_fr = {
-        "bot_response": "<emoji document_id=5206607081334906820>✔️</emoji> <b>Le bot utilisateur fonctionne!</b>",
+        "bot_response": "<emoji document_id=5206607081334906820>✔️</emoji> <b>Le bot utilisateur fonctionne!</b>\n<i>~ {uptime}</i>",
         "_cls_doc": "Module pour vérifier si le bot utilisateur fonctionne.",
         "_cmd_doc_bot": "- utilisez cette commande pour vérifier le fonctionnement."
     }
 
     strings_it = {
-        "bot_response": "<emoji document_id=5206607081334906820>✔️</emoji> <b>Il bot utente funziona!</b>",
+        "bot_response": "<emoji document_id=5206607081334906820>✔️</emoji> <b>Il bot utente funziona!</b>\n<i>~ {uptime}</i>",
         "_cls_doc": "Modulo per verificare se l'utente bot funziona.",
         "_cmd_doc_bot": "- usa questo comando per controllare il funzionamento."
     }
 
     strings_de = {
-        "bot_response": "<emoji document_id=5206607081334906820>✔️</emoji> <b>Benutzerbot funktioniert!</b>",
+        "bot_response": "<emoji document_id=5206607081334906820>✔️</emoji> <b>Benutzerbot funktioniert!</b>\n<i>~ {uptime}</i>",
         "_cls_doc": "Modul zur Überprüfung der Funktionsfähigkeit des Benutzerbots.",
         "_cmd_doc_bot": "- Verwenden Sie diesen Befehl, um die Funktionsfähigkeit zu überprüfen."
     }
 
     strings_rt = {
-        "bot_response": "<emoji document_id=5206607081334906820>✔️</emoji> <b>Benutzerbot funktioniert!</b>",
+        "bot_response": "<emoji document_id=5206607081334906820>✔️</emoji> <b>Benutzerbot funktioniert!</b>\n<i>~ {uptime}</i>",
         "_cls_doc": "Modul zur Überprüfung der Funktionsfähigkeit des Benutzerbots.",
         "_cmd_doc_bot": "- Verwenden Sie diesen Befehl, um die Funktionsfähigkeit zu überprüfen."
     }
 
     strings_uz = {
-        "bot_response": "<emoji document_id=5206607081334906820>✔️</emoji> <b>Foydalanuvchi bot ishlayapti!</b>",
+        "bot_response": "<emoji document_id=5206607081334906820>✔️</emoji> <b>Foydalanuvchi bot ishlayapti!</b>\n<i>~ {uptime}</i>",
         "_cls_doc": "Foydalanuvchi botining ishlashini tekshirish uchun modul.",
         "_cmd_doc_bot": "- ishlashini tekshirish uchun ushbu buyruqni ishlating."
     }
 
     strings_es = {
-        "bot_response": "<emoji document_id=5206607081334906820>✔️</emoji> <b>¡El bot de usuario está funcionando!</b>",
+        "bot_response": "<emoji document_id=5206607081334906820>✔️</emoji> <b>¡El bot de usuario está funcionando!</b>\n<i>~ {uptime}</i>",
         "_cls_doc": "Módulo para verificar si el bot de usuario está funcionando.",
         "_cmd_doc_bot": "- utilice este comando para verificar el funcionamiento."
     }
 
     strings_kk = {
-        "bot_response": "<emoji document_id=5206607081334906820>✔️</emoji> <b>Юзербот жұмыс істеп тұр!</b>",
+        "bot_response": "<emoji document_id=5206607081334906820>✔️</emoji> <b>Юзербот жұмыс істеп тұр!</b>\n<i>~ {uptime}</i>",
         "_cls_doc": "Юзерботтың жұмыс істеп тұрғанын тексеру модулі.",
         "_cmd_doc_bot": "- жұмыс істеуін тексеру үшін осы команданы қолданыңыз."
     }
 
     strings_tt = {
-        "bot_response": "<emoji document_id=5206607081334906820>✔️</emoji> <b>Юзербот эшли!</b>",
+        "bot_response": "<emoji document_id=5206607081334906820>✔️</emoji> <b>Юзербот эшли!</b>\n<i>~ {uptime}</i>",
         "_cls_doc": "Юзерботның эшләвен тикшерү модуле.",
         "_cmd_doc_bot": "- эшләүне тикшерү өчен бу команданы кулланыгыз."
     }
@@ -76,4 +77,6 @@ class BotCheck(loader.Module):
     @loader.command()
     async def bot(self, message: Message):
         """- use this command to check the functionality."""
-        await utils.answer(message, self.strings["bot_response"])
+        uptime = utils.formatted_uptime()
+        response = self.strings["bot_response"].format(uptime=uptime)
+        await utils.answer(message, response)
